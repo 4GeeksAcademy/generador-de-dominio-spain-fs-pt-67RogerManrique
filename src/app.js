4c96543828259f6ee83e1fc53f74f9dc83847efc
Roger Manrique <159472606+RAML84@users.noreply.github.com>
2024-05-19 10:04:47 +0000

    domain-generator

--- src/app.js
+++ src/app.js
@@ -9,11 +9,11 @@ window.onload = function() {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
-  let domains = [];
+  let domains = [""];
 
-  for (let i = 0; 1 < pronoun.length; i++) {
-    for (let j = 0; 1 < adj.length; j++) {
-      for (let k = 0; 1 < noun.length; k++) {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
         console.log(pronoun[i] + adj[j] + noun[k]);
         const domain = pronoun[i] + adj[j] + noun[k] + ".com";
         domains.push(domain);
